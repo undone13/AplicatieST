630ab99e6b2fe8fb052ead8265a6e67e44ecb464
Bogdan Ichim <bogdan_ichim@lovecat.com>
2022-07-06 20:56:44 +0300

    Add project files.

---
+++ chartFinal/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace chartFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_date = new System.Windows.Forms.TabPage();
+            this.tab_ora = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tab_univ = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tab_csv = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tab_image = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tab_chart = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tab_ora.SuspendLayout();
+            this.tab_univ.SuspendLayout();
+            this.tab_csv.SuspendLayout();
+            this.tab_image.SuspendLayout();
+            this.tab_chart.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab_date);
+            this.tabControl1.Controls.Add(this.tab_ora);
+            this.tabControl1.Controls.Add(this.tab_univ);
+            this.tabControl1.Controls.Add(this.tab_csv);
+            this.tabControl1.Controls.Add(this.tab_image);
+            this.tabControl1.Controls.Add(this.tab_chart);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(755, 367);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tab_date
+            // 
+            this.tab_date.Location = new System.Drawing.Point(4, 22);
+            this.tab_date.Name = "tab_date";
+            this.tab_date.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_date.Size = new System.Drawing.Size(747, 341);
+            this.tab_date.TabIndex = 0;
+            this.tab_date.Text = "Date personale";
+            this.tab_date.UseVisualStyleBackColor = true;
+            // 
+            // tab_ora
+            // 
+            this.tab_ora.Controls.Add(this.button2);
+            this.tab_ora.Controls.Add(this.button1);
+            this.tab_ora.Location = new System.Drawing.Point(4, 22);
+            this.tab_ora.Name = "tab_ora";
+            this.tab_ora.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_ora.Size = new System.Drawing.Size(747, 341);
+            this.tab_ora.TabIndex = 1;
+            this.tab_ora.Text = "Schimbare format ora";
+            this.tab_ora.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "FORMAT FARA AM/PM";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FORMAT AM/PM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tab_univ
+            // 
+            this.tab_univ.Controls.Add(this.button3);
+            this.tab_univ.Location = new System.Drawing.Point(4, 22);
+            this.tab_univ.Name = "tab_univ";
+            this.tab_univ.Size = new System.Drawing.Size(747, 341);
+            this.tab_univ.TabIndex = 2;
+            this.tab_univ.Text = "Universitati";
+            this.tab_univ.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 20);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Arata Universitati";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tab_csv
+            // 
+            this.tab_csv.Controls.Add(this.button4);
+            this.tab_csv.Location = new System.Drawing.Point(4, 22);
+            this.tab_csv.Name = "tab_csv";
+            this.tab_csv.Size = new System.Drawing.Size(747, 341);
+            this.tab_csv.TabIndex = 3;
+            this.tab_csv.Text = "csv";
+            this.tab_csv.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 67);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "CSV";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tab_image
+            // 
+            this.tab_image.Controls.Add(this.button5);
+            this.tab_image.Controls.Add(this.textBox1);
+            this.tab_image.Location = new System.Drawing.Point(4, 22);
+            this.tab_image.Name = "tab_image";
+            this.tab_image.Size = new System.Drawing.Size(747, 341);
+            this.tab_image.TabIndex = 4;
+            this.tab_image.Text = "Imagini";
+            this.tab_image.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(170, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 20);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Cauta";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tab_chart
+            // 
+            this.tab_chart.Controls.Add(this.chart1);
+            this.tab_chart.Location = new System.Drawing.Point(4, 22);
+            this.tab_chart.Name = "tab_chart";
+            this.tab_chart.Size = new System.Drawing.Size(747, 341);
+            this.tab_chart.TabIndex = 5;
+            this.tab_chart.Text = "Chart";
+            this.tab_chart.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Teal;
+            series1.Legend = "Legend1";
+            series1.Name = "Numar Facultati";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(741, 335);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 384);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tab_ora.ResumeLayout(false);
+            this.tab_univ.ResumeLayout(false);
+            this.tab_csv.ResumeLayout(false);
+            this.tab_image.ResumeLayout(false);
+            this.tab_image.PerformLayout();
+            this.tab_chart.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab_date;
+        private System.Windows.Forms.TabPage tab_ora;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tab_univ;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage tab_csv;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabPage tab_image;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tab_chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ chartFinal/Form1.cs
@@ -0,0 +1,336 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace chartFinal
+{
+    public partial class Form1 : Form
+    {
+        List<Universitate> jsonData;
+        TabPage tb_chart;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DATE PERSONALE----------------------------------------------------------------    
+
+            string date = Properties.Resources.date_personale;
+            string[] linii = date.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Label nume_prenume = new Label()
+            {
+                Text = linii[0] + " " + linii[1],
+                Location = new Point(10, 10),
+                Size = new Size(300, 20)
+            };
+            Label nr_telefon = new Label()
+            {
+                Text = linii[2],
+                Location = new Point(10, 30),
+                Size = new Size(300, 20)
+            };
+            tab_date.Controls.Add(nume_prenume);
+            tab_date.Controls.Add(nr_telefon);
+
+            chart1.Enabled = false;
+            chart1.Visible = false;
+            //--------------------------------------------------------------------------------
+        }
+
+        //SCHIMBARE ORA---------------------------------------------------------------------
+        RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Control Panel\International", true);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart req = new ThreadStart(Thread);
+            Thread reqThread = new Thread(req);
+            reqThread.Start();
+            SchimbareOra("h:mm tt");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ThreadStart req = new ThreadStart(Thread);
+            Thread reqThread = new Thread(req);
+            reqThread.Start();
+            SchimbareOra("h:mm");
+        }
+
+        public void SchimbareOra(string format)
+        {
+            var nr = 0;
+            regKey.SetValue("sShortTime", format);
+            var winExp = Process.GetProcessesByName("explorer");
+            foreach (var exp in winExp)
+            {
+                exp.Kill();
+                Process.Start(exp.ProcessName);
+            }
+        }
+        public void Thread()
+        {
+            Label lbl = new Label();
+            lbl.Location = new Point(180, 10);
+            lbl.Text = System.DateTime.Now.ToString(regKey.GetValue("sShortTime").ToString());
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                tab_ora.Controls.Add(lbl);
+            }
+            ));
+        }
+
+        //UNIVERSITATI------------------------------------------------------------------------
+
+        public void ThreadUniversitati()
+        {
+            var client = new HttpClient();
+            var ep = new Uri("http://universities.hipolabs.com/search?name=middle");
+            var res = client.GetAsync(ep).Result;
+            var JSON = res.Content.ReadAsStringAsync().Result;
+            jsonData = JsonConvert.DeserializeObject<List<Universitate>>(JSON);
+
+            if (res != null)
+            {
+                var dist = 0; var limit = 500;
+                foreach (var item in jsonData)
+                {
+                    Label lb1 = new Label();
+                    lb1.Text = item.name;
+                    lb1.Location = new Point(10, dist + 10);
+                    lb1.AutoSize = true;
+                    lb1.Click += (s, e) =>
+                    {
+                        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        {
+                            string url = item.web_pages[0];
+                            url = url.Replace("&", "^&");
+                            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                        }
+                    };
+
+                    Label lb2 = new Label();
+                    lb2.Text = item.country;
+                    lb2.Location = new Point(10, dist + 30);
+                    lb2.AutoSize = true;
+                    lb2.Click += (s, e) =>
+                    {
+                        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        {
+                            string url = item.web_pages[0];
+                            url = url.Replace("&", "^&");
+                            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                        }
+                    };
+
+                    Label lb3 = new Label();
+                    lb3.Text = item.alpha_two_code;
+                    lb3.Location = new Point(10, dist + 50);
+                    lb3.AutoSize = true;
+                    lb3.Click += (s, e) =>
+                    {
+                        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        {
+                            string url = item.web_pages[0];
+                            url = url.Replace("&", "^&");
+                            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                        }
+                    };
+
+                    dist += 80;
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        tab_univ.Controls.Add(lb1); tab_univ.Controls.Add(lb2); tab_univ.Controls.Add(lb3);
+                    }
+                    ));
+                }
+            }
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            tab_univ.AutoScroll = true;
+            ThreadStart req = new ThreadStart(ThreadUniversitati);
+            Thread reqThread = new Thread(req);
+            reqThread.Start();
+
+
+            //CHART-----------------------------------------------------------------
+            if (jsonData == null)
+                System.Threading.Thread.Sleep(1000);
+
+            chart1.Enabled = true;
+            chart1.Visible = true;
+
+            string[] str = new string[jsonData.Count];
+            string[] distStr = new string[jsonData.Count];
+
+
+            for (int i = 0; i < jsonData.Count; i++)
+            {
+                str[i] = jsonData[i].country;
+            }
+            distStr = str.Distinct().ToArray();
+            for (int i = 0; i < distStr.Length; i++)
+            {
+                int nr = str.Count(x => x == distStr[i]);
+                chart1.Series[0].Points.AddXY(distStr[i].ToString(), nr);
+            }
+            //-----------------------------------------------------------------------
+
+        }
+        //-----------------------------------------------------------------------------------
+
+        //TEXT FILE-------------------------------------------------------------------------
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (jsonData != null)
+            {
+                string fileName = "date_universitati.txt";
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    foreach (var item in jsonData)
+                    {
+                        sw.WriteLine(item.name + "\t" + item.country + "\t" + item.alpha_two_code + "\t" + item.web_pages[0]);
+                    }
+                }
+                Process.Start(fileName);
+            }
+            else
+            {
+                MessageBox.Show("Nu exista date");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string html = GetHtmlCode();
+            if (html == "")
+            {
+                MessageBox.Show("Scrie ce vrei sa cauti in TextBox");
+            }
+            string text_anterior = textBox1.Text;
+            List<string> urls = GetUrls(html);
+            var dist = 5;
+            int i = 0, coloana = 5;
+
+            var panel1 = new Panel();
+            panel1.Location = new Point(15, 44);
+            panel1.Size = new Size(735, 348);
+            tab_image.Controls.Add(panel1);
+
+            foreach (var item in urls)
+            {
+                var pb = new PictureBox();
+                SaveImage(item, "image" + i, ImageFormat.Png);
+
+                if (coloana + 130 > panel1.Width)
+                {
+                    dist += 140;
+                    coloana = 0;
+                }
+                pb.Load(item);
+                pb.Size = new Size(130, 130);
+                pb.Location = new Point(coloana, dist);
+                pb.SizeMode = PictureBoxSizeMode.Normal;
+                panel1.Controls.Add(pb);
+                coloana += 140; i++;
+            }
+            panel1.AutoScroll = true;
+        }
+        private string GetHtmlCode()
+        {
+            if (textBox1.Text != "")
+            {
+                string url = "https://www.google.com/search?q=" + textBox1.Text + "&tbm=isch";
+                string data = "";
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    if (dataStream == null)
+                        return "";
+                    using (var sr = new StreamReader(dataStream))
+                    {
+                        data = sr.ReadToEnd();
+                    }
+                }
+                return data;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        private List<string> GetUrls(string html)
+        {
+            var urls = new List<string>();
+
+            int ndx = html.IndexOf("src=\"https", StringComparison.Ordinal);
+
+            while (ndx >= 0)
+            {
+                ndx = html.IndexOf("\"", ndx + 4, StringComparison.Ordinal);
+                ndx++;
+                int ndx2 = html.IndexOf("\"", ndx, StringComparison.Ordinal);
+                string url = html.Substring(ndx, ndx2 - ndx);
+                urls.Add(url);
+                ndx = html.IndexOf("src=\"https", ndx2, StringComparison.Ordinal);
+            }
+            return urls;
+        }
+
+        private void SaveImage(string imageUrl, string filename, ImageFormat format)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(imageUrl);
+            Bitmap bitmap; bitmap = new Bitmap(stream);
+
+            if (bitmap != null)
+            {
+                bitmap.Save(filename, format);
+            }
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+        }
+    }
+
+        public class Universitate
+        {
+            public string[] web_pages { get; set; }
+            public string name { get; set; }
+            public string country { get; set; }
+            public string alpha_two_code { get; set; }
+        }
+
+}
